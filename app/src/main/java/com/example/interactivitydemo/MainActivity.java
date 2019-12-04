807106b4d45be029d1603718592074596dec011f
Ashutosh Nayak <nayakashutosh99@gmail.com>
2019-12-04 21:31:32 +0530

    added message in login page

--- app/src/main/java/com/example/interactivitydemo/MainActivity.java
+++ app/src/main/java/com/example/interactivitydemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +15,12 @@ public class MainActivity extends AppCompatActivity {
     public void clickFunction(View view){
         EditText nameEditText2= (EditText)findViewById(R.id.nameEditText2);
         EditText nameEditText3= (EditText)findViewById(R.id.nameEditText3);
+        EditText nameEditText4= (EditText)findViewById(R.id.nameEditText4);
         Log.i( "Info", "It Worked!!");
         Log.i("Email",nameEditText2.getText().toString());
         Log.i("Password", nameEditText3.getText().toString());
+        Log.i("Name",nameEditText4.getText().toString());
+        Toast.makeText(this,"Hello "+nameEditText4.getText().toString(),Toast.LENGTH_SHORT).show();
     }
 
     @Override
